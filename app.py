0ad0411c8430e89aebd9f682a1cecb4f94377b61
Austin Ramsey <ramseydevelop@gmail.com>
2020-09-27 12:15:50 -0400

    Required Files Added
    Added all the required files to complete website from other github project

--- app.py
+++ app.py
@@ -3,55 +3,204 @@ from datetime import date,time, datetime
 import sqlite3 as sql
 import os
 
-class User:
-    def __init__(self, id, username, password):
-        self.id = id
-        self.username = username
-        self.password = password
+# Create Database if it doesnt exist
+if not os.path.isfile('database.db'):
+  conn = sql.connect('database.db')
+  conn.execute('CREATE TABLE IF NOT EXISTS Users (Name TEXT NOT NULL, Email TEXT NOT NULL, Password TEXT NOT NULL, Contact INTEGER NOT NULL)')
+  conn.close()
 
-    def __repr__(self):
-        return f'<User: {self.username}>'
-
-users = []
-users.append(User(id=1, username='Admin', password='admin'))
-users.append(User(id=2, username='austinr', password='password'))
+app = Flask(__name__, static_url_path='/assets',
+            static_folder='assets',
+            template_folder='./')
 
 
-app = Flask(__name__)
-app.secret_key = 'somesecretkeythatonlyishouldknow'
-
-@app.before_request
-def before_request():
-    g.user = None
-
-    if 'user_id' in session:
-        user = [x for x in users if x.id == session['user_id']][0]
-        g.user = user
-        
 @app.route('/')
+def root():
+    session['logged_out'] = 1
+    return render_template('index.html')
+
+
+@app.route('/index.html')
 def index():
-    return 'Welcome'
+    return render_template('index.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+
+@app.route('/header_page.html')
+def header_page():
+    return render_template('header_page.html')
+
+
+@app.route('/menu-bar-charity.html')
+def menu_bar_charity():
+    return render_template('menu-bar-charity.html')
+
+
+@app.route('/footer.html')
+def footer():
+    return render_template('footer.html')
+
+
+@app.route('/sidebar.html')
+def sidebar():
+    return render_template('sidebar.html')
+
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/our-causes.html')
+def our_causes():
+    return render_template('our-causes.html')
+
+
+@app.route('/about-us.html')
+def about_us():
+    return render_template('about-us.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
     if request.method == 'POST':
-        session.pop('user_id', None)
-
-        username = request.form['username']
+        nm = request.form['nm']
+        contact = request.form['contact']
+        email = request.form['email']
         password = request.form['password']
-        
-        user = [x for x in users if x.username == username][0]
-        if user and user.password == password:
-            session['user_id'] = user.id
-            return redirect(url_for('profile'))
 
-        return redirect(url_for('login'))
+        with sql.connect("database.db") as con:
+            cur = con.cursor()
+            # check if User already present
+            cur.execute("SELECT Email FROM Users WHERE Email=(?)", [(email)])
+            data = cur.fetchall()
+            if len(data) > 0:
+                print('User already exists')
+                user_exists = 1
+            else:
+                print("User not found, register new user")
+                user_exists = 0
+                cur.execute("INSERT INTO Users (Name,Email,Password,Contact) VALUES (?,?,?,?)",
+                            (nm, email, password, contact))
 
-    return render_template('login.html')
+    return render_template('login.html', user_exists=user_exists, invalid=None, logged_out=None)
 
+
+@app.route('/login.html', methods=['GET', 'POST'])
+def login():
+    invalid = None
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        with sql.connect("database.db") as con:
+            cur = con.cursor()
+            # Validate user credentails from database
+            cur.execute("SELECT Email FROM Users WHERE Email=(?) AND Password=(?)", [(email), (password)])
+            data = cur.fetchall()
+            if len(data) > 0:
+                print('Login Success')
+                # Fetch name of user
+                cur.execute("SELECT Name FROM Users WHERE Email=(?) AND Password=(?)", [(email), (password)])
+                nm = cur.fetchall()
+                nm = nm[0][0]
+                # Store User details in Session and log in user
+                session['nm'] = nm
+                session['email'] = email
+                session['logged_out'] = None
+                return redirect(url_for('donate'))
+            else:
+                print("Invalid Login")
+                invalid = 1
+    return render_template('login.html', user_exists=None, invalid=invalid, logged_out=None)
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    session['logged_out'] = 1
+    print('Session Cleared and Logged Out')
+    return render_template('index.html')
+
+
+@app.route('/donate')
+def donate():
+    # If Logged Out, Redirect to Log In page
+    if session['logged_out']:
+        return render_template('login.html', logged_out=1, user_exists=None, invalid=None)
+    nm = session['nm']
+    email = session['email']
+    return render_template('donate.html', nm=nm, email=email)
+
+
+# insert values into table
+@app.route('/donation', methods=['POST', 'GET'])
+def donation():
+    # If Logged Out, Redirect to Log In page
+    if session['logged_out']:
+        return render_template('login.html', logged_out=1, user_exists=None, invalid=None)
+    if request.method == 'POST':
+        nm = session['nm']
+        email = session['email']
+        amt = request.form['amt']
+        today = datetime.now()
+        today = today.strftime("%d-%m-%Y" + "," + "%H:%M")
+
+        with sql.connect("database.db") as con:
+            cur = con.cursor()
+            # check if already donated. If already donated, add donation. Else create new donation
+            cur.execute("SELECT Email FROM Donors WHERE Email=(?)", [(email)])
+            data = cur.fetchall()
+            if len(data) > 0:
+                cur.execute("UPDATE Donors SET Amount=Amount+(?) WHERE Email=(?)", [(amt), (email)])
+            else:
+                cur.execute("INSERT INTO Donors (Name,Amount,Email,timestamp) VALUES (?,?,?,?)",
+                            (nm, amt, email, today))
+            con.commit()
+
+            # Greeting
+            msg = "Thank You for Donating"
+            for row in cur.execute("SELECT Amount FROM Donors WHERE Email=(?)", [(email)]):
+                Amount = row
+        return render_template("greeting.html", msg=msg, nm=nm, Amount=Amount, today=today, email=email)
+        con.close()
+
+
+# Display List of Donations
+@app.route('/list1')
+def list1():
+    # If Logged Out, Redirect to Log In page
+    if session['logged_out']:
+        return render_template('login.html', logged_out=1, user_exists=None, invalid=None)
+    con = sql.connect("database.db")
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+    cur.execute("SELECT * FROM Donors")
+
+    rows = cur.fetchall();
+    return render_template("list1.html", rows=rows)
+
+
+# Display Profile
 @app.route('/profile')
 def profile():
-    if not g.user:
-        return redirect(url_for('login'))
+    # If Logged Out, Redirect to Log In page
+    if session['logged_out']:
+        return render_template('login.html', logged_out=1, user_exists=None, invalid=None)
+    nm = session['nm']
+    email = session['email']
+    with sql.connect("database.db") as con:
+        cur = con.cursor()
+        # Fetch details of user
+        cur.execute("SELECT Contact FROM Users WHERE Email=(?)", [(email)])
+        contact = cur.fetchall()
+        contact = contact[0][0]
 
-    return render_template('profile.html')
+        cur.execute("SELECT Password FROM Users WHERE Email=(?)", [(email)])
+        password = cur.fetchall()
+        password = password[0][0]
+    return render_template("profile.html", nm=nm, email=email, contact=contact, password=password)
+
+
+if __name__ == '__main__':
+    app.secret_key = ".."
+    app.run()
